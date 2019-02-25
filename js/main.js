59810a7b2f6978bfe17e503eb3cadb168cfe46f0
Sylvebois <thomasghaye@hotmail.com>
2019-02-25 14:11:46 +0100

    Manage window.onresize

--- js/main.js
+++ js/main.js
@@ -3,9 +3,15 @@ import CanvasManager from './classes/CanvasManager.js';
 let can = new CanvasManager();
 can.canvases.get('ui')[0].setAttribute('height', 100);
 
-can.canvases.get('ui')[0].addEventListener('click', e => {
+can.canvases.get('ui')[0].onclick = e => {
   if(can.state === 'game') {
     let size = (e.target.getAttribute('height') < can.size)? can.size : 100;
     e.target.setAttribute('height', size);
   }
-});
+};
+
+window.onresize = e => {
+  let uiSize = can.canvases.get('ui')[0].getAttribute('height');
+  can.setSize();
+  can.canvases.get('ui')[0].setAttribute('height', uiSize);
+};
