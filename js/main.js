c011d1c76470b9aea7e8091c7e80367b8928730b
Sylvebois <thomasghaye@hotmail.com>
2019-02-26 15:55:46 +0100

    Settled the base for the click events depending on the state

--- js/main.js
+++ js/main.js
@@ -7,7 +7,26 @@ let view = new View(can.canvases, can.ratio);
 
 can.canvases.get('ui')[0].onclick = e => {
   if (can.state === 'start') {
+    let beginMenuPos = view.menuTextPos.get('Commencer');
+    let continueMenuPos = view.menuTextPos.get('Continuer');
+    let creditMenuPos = view.menuTextPos.get('Crédits');
 
+    if (e.clientX >= beginMenuPos[0] && e.clientX <= beginMenuPos[1] && e.clientY >= beginMenuPos[2] && e.clientY <= beginMenuPos[3]) {
+      can.state = 'story';
+    }
+    else if (continueMenuPos && e.clientX >= continueMenuPos[0] && e.clientX <= continueMenuPos[1] && e.clientY >= continueMenuPos[2] && e.clientY <= continueMenuPos[3]) {
+      can.state = 'load';
+    }
+    else if (e.clientX >= creditMenuPos[0] && e.clientX <= creditMenuPos[1] && e.clientY >= creditMenuPos[2] && e.clientY <= creditMenuPos[3]) {
+      can.state = 'credits';
+    }
+  }
+  else if (can.state === 'credits') {
+    can.state = 'start';
+    view.drawHomeScreen();
+  }
+  else if (can.state === 'story') {
+    can.state = 'game';
   }
   else if (can.state === 'game') {
     let size = (e.target.getAttribute('height') < can.size)? can.size : TILESIZE * can.ratio;
