5171bc550411d49aaefddab28c5adee483e165a0
Sylvebois <thomasghaye@hotmail.com>
2019-04-30 14:11:22 +0200

    Changed image path

--- js/classes/GameManager.js
+++ js/classes/GameManager.js
@@ -10,7 +10,7 @@ export default class GameManager {
     let bossLvl = (dungeonLvl === 9)? true : false;
 
     if (this.lvl >= 0 && this.lvl < 10) {
-      this.imgTileset = '../../images/crossRoad.png';
+      this.imgTileset = 'crossRoad.png';
       this.enemySet = [
         { name: 'Loup', imgX: 6 },
         { name: 'Mouche géante', imgX: 7 },
@@ -21,23 +21,23 @@ export default class GameManager {
       console.log('La croisée des chemins');
     }
     else if (this.lvl >= 10 && this.lvl < 20) {
-      this.imgTileset = '../../images/farDesert.png';
+      this.imgTileset = 'farDesert.png';
       console.log('Le désert retiré');
     }
     else if (this.lvl >= 20 && this.lvl < 30) {
-      this.imgTileset = '../../images/multiplicatedWoods.png';
+      this.imgTileset = 'multiplicatedWoods.png';
       console.log('La forêt multipliée');
     }
     else if (this.lvl >= 30 && this.lvl < 40) {
-      this.imgTileset = '../../images/dividedValley.png';
+      this.imgTileset = 'dividedValley.png';
       console.log('La vallée divisée');
     }
     else if (this.lvl >= 40 && this.lvl < 50) {
-      this.imgTileset = '../../images/dungeonOfPower.png';
+      this.imgTileset = 'dungeonOfPower.png';
       console.log('Le donjon de puissance');
     }
     else if (this.lvl >= 50 && this.lvl < 60) {
-      this.imgTileset = '../../images/rootOfTheWorld.png';
+      this.imgTileset = 'rootOfTheWorld.png';
       console.log('Les racines du monde');
     }
 
@@ -53,8 +53,8 @@ export default class GameManager {
 
       for(let j = 0; j < this.nbTilesPerLine; j++) {
         lvlMap[i][j] = {
-          ground: new Tile(i, j, this.imgTileset),
-          character: null
+          backPart: new Tile(i, j, this.imgTileset),
+          frontPart: null
         };
       }
     }
@@ -66,17 +66,19 @@ export default class GameManager {
 }
 
 class Tile {
-  constructor(x = 0, y = 0, imgPath = '', imgX = 0, imgY = 0, canWalkOnIt = true) {
+  constructor(x = 0, y = 0, img = '', imgX = 0, imgY = 0, canWalkOnIt = true) {
     this.x = x;
     this.y = y;
-    this.img = imgPath;
+    this.img = img;
+    this.imgX = imgX;
+    this.imgY = imgY;
     this.canWalkOnIt = canWalkOnIt;
   }
 }
 
 class Hero extends Tile {
-  constructor(x = 0, y = 0, imgPath = '../../images/hero.png', imgX = 0, imgY = 0, canWalkOnIt = false, name = 'John Doe') {
-    super(x, y, imgPath, imgX, imgY, canWalkOnIt);
+  constructor(x = 0, y = 0, img = 'hero.png', imgX = 0, imgY = 0, canWalkOnIt = false, name = 'John Doe') {
+    super(x, y, img, imgX, imgY, canWalkOnIt);
     this.live = 3;
     this.name = name;
   }
@@ -86,8 +88,8 @@ class Hero extends Tile {
 }
 
 class Monster extends Hero {
-  constructor(x = 0, y = 0, imgPath = '', imgX = 0, imgY = 1, canWalkOnIt = false, name = 'Ennemi', isBoss = false) {
-    super(x, y, imgPath, imgX = 0, imgY = 0, canWalkOnIt, name);
+  constructor(x = 0, y = 0, img = '', imgX = 0, imgY = 1, canWalkOnIt = false, name = 'Ennemi', isBoss = false) {
+    super(x, y, img, imgX = 0, imgY = 0, canWalkOnIt, name);
     this.isBoss = isBoss;
     this.nbOperation = (this.isBoss)? 2 : 1;
     this.live = (this.isBoss)? 5 : 1;
