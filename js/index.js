1b47cb3a82e75ff1caf98329432cfe6c8a0a1221
Sylvebois <thomasghaye@hotmail.com>
2019-08-21 16:24:29 +0200

    Resizeable scenes

--- js/index.js
+++ js/index.js
@@ -10,6 +10,42 @@ import Model from './Model.js';
 
 /* template from : https://phasertutorials.com/creating-a-phaser-3-template-part-1/ */
 
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Boot');
+  }
+}
+
+window.game = new Game();
+window.focus();
+
+resize();
+window.addEventListener("resize", resize, false);
+
+function resize() {
+  let canvas = document.querySelector("canvas");
+  let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
+  let windowRatio = windowWidth / windowHeight;
+  let gameRatio = config.width / config.height;
+  if (windowRatio < gameRatio) {
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = (windowWidth / gameRatio) + "px";
+  }
+  else {
+      canvas.style.width = (windowHeight * gameRatio) + "px";
+      canvas.style.height = windowHeight + "px";
+  }
+}
 
 /*
 import CanvasManager from './classes/CanvasManager.js';
