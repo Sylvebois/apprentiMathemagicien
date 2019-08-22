6c5e536874ec48899263452f797288b35c704522
Sylvebois <thomasghaye@hotmail.com>
2019-08-22 11:52:36 +0200

    Made preload scene responsive

--- js/Scenes/PreloaderScene.js
+++ js/Scenes/PreloaderScene.js
@@ -10,17 +10,15 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload () {
-    // add logo image
-    this.add.image(400, 200, 'logo');
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
 
     // display progress bar
     var progressBar = this.add.graphics();
     var progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
-    progressBox.fillRect(240, 270, 320, 50);
+    progressBox.fillRect(width / 2 - 160, height / 2 - 25, 320, 50);
 
-    var width = this.cameras.main.width;
-    var height = this.cameras.main.height;
     var loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
@@ -54,12 +52,16 @@ export default class PreloaderScene extends Phaser.Scene {
     });
     assetText.setOrigin(0.5, 0.5);
 
+    // add logo image
+    let logo = this.add.image(0, 0, 'logo');
+    logo.setPosition(width / 2, loadingText.y - logo.height);
+
     // update progress bar
     this.load.on('progress', function (value) {
       percentText.setText(parseInt(value * 100) + '%');
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
-      progressBar.fillRect(250, 280, 300 * value, 30);
+      progressBar.fillRect(width / 2 - 150, height / 2 - 15, 300 * value, 30);
     });
 
     // update file progress text
@@ -80,12 +82,11 @@ export default class PreloaderScene extends Phaser.Scene {
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
     // load assets needed in our game
-    this.load.image('blueButton1', 'assets/ui/blue_button02.png');
+   /* this.load.image('blueButton1', 'assets/ui/blue_button02.png');
     this.load.image('blueButton2', 'assets/ui/blue_button03.png');
-    this.load.image('phaserLogo', 'assets/logo.png');
     this.load.image('box', 'assets/ui/grey_box.png');
     this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
-    this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
+    this.load.audio('bgMusic', ['assets/TownTheme.mp3']);*/
   }
 
   ready () {
