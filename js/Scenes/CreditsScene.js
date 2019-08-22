a380dcbdd5e37ddf7f2583180ed61e7234ebf2bd
Sylvebois <thomasghaye@hotmail.com>
2019-08-22 15:52:40 +0200

    Customized credit scene

--- js/Scenes/CreditsScene.js
+++ js/Scenes/CreditsScene.js
@@ -2,46 +2,65 @@ import * as PHASER from '../phaser.min.js';
 import config from '../Config/config.js';
 
 export default class CreditsScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Credits');
   }
 
-  create () {
+  create() {
+    // Background
+    this.add.image(config.width / 2, config.height / 2, 'bgScroll');
+
+    //Credits
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
-    this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
+    this.madeByText = this.add.text(0, 0, 'Created By: Sylvebois', { fontSize: '26px', fill: '#fff' });
+    this.otherThanksText = this.add.text(0, 0, 'Graphics and Sounds: Open Game Art\r\nEngine: Phaser 3', { fontSize: '26px', fill: '#fff' });
 
-    Phaser.Display.Align.In.Center(
-      this.creditsText,
-      this.zone
-    );
+    this.madeByText.alpha = 0;
+    this.otherThanksText.alpha = 0;
 
-    Phaser.Display.Align.In.Center(
-      this.madeByText,
-      this.zone
-    );
+    this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
 
-    this.madeByText.setY(1000);
+    Phaser.Display.Align.In.Center(this.creditsText, this.zone);
+    Phaser.Display.Align.In.Center(this.madeByText, this.zone);
+    Phaser.Display.Align.In.Center(this.otherThanksText, this.zone);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
-      y: -100,
+      y: 10,
+      alpha: 0,
       ease: 'Power1',
       duration: 3000,
       delay: 1000,
       onComplete: function () {
         this.destroy;
-      }
+        this.madeByText.alpha = 1;
+        this.madeByTween.play();
+      }.bind(this)
     });
 
     this.madeByTween = this.tweens.add({
       targets: this.madeByText,
-      y: -300,
-      ease: 'Power1',
-      duration: 8000,
-      delay: 1000,
+      y: 10,
+      alpha: 0,
+      duration: 3000,
+      delay: 500,
+      paused: true,
       onComplete: function () {
-        this.madeByTween.destroy;
+        this.destroy;
+        this.otherThanksText.alpha = 1;
+        this.otherThanksTween.play();
+      }.bind(this)
+    });
+
+    this.otherThanksTween = this.tweens.add({
+      targets: this.otherThanksText,
+      y: 10,
+      alpha: 0,
+      duration: 3000,
+      delay: 500,
+      paused: true,
+      onComplete: function () {
+        this.otherThanksTween.destroy;
         this.scene.start('Title');
       }.bind(this)
     });
