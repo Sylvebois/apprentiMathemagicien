4f7125d061708f83d912b18941904c8ac6dba1b8
Sylvebois <thomasghaye@hotmail.com>
2019-09-04 15:56:46 +0200

    Watching keyboard events

--- js/Scenes/GameScene.js
+++ js/Scenes/GameScene.js
@@ -2,16 +2,55 @@ import * as PHASER from '../phaser.min.js';
 import config from '../Config/config.js';
 
 export default class GameScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Game');
   }
 
-  preload () {
-    // load images
-    this.load.image('logo', 'assets/logo.png');
+  create() {
+    //Checking for input (keyboard and mouse)
+    this.input.on('pointerdown', this.action, this);
+    this.cursors = this.keysToWatch();
   }
 
-  create () {
-    this.add.image(400, 300, 'logo');
+  update() {
+
+  }
+
+  action() {
+    if (this.state === 'fight') {
+
+    }
+    else {
+
+    }
+  }
+
+  keysToWatch() {
+    let arrowKeys = this.input.keyboard.createCursorKeys();
+    let otherKeys = this.input.keyboard.addKeys({
+      'zero' : Phaser.Input.Keyboard.KeyCodes.ZERO,
+      'one' : Phaser.Input.Keyboard.KeyCodes.ONE,
+      'two' : Phaser.Input.Keyboard.KeyCodes.TWO,
+      'three' : Phaser.Input.Keyboard.KeyCodes.THREE,
+      'four' : Phaser.Input.Keyboard.KeyCodes.FOUR,
+      'five' : Phaser.Input.Keyboard.KeyCodes.FIVE,
+      'six' : Phaser.Input.Keyboard.KeyCodes.SIX,
+      'seven' : Phaser.Input.Keyboard.KeyCodes.SEVEN,
+      'eight' : Phaser.Input.Keyboard.KeyCodes.EIGHT,
+      'nine' : Phaser.Input.Keyboard.KeyCodes.NINE,
+      'numZero' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_ZERO,
+      'numOne' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_ONE,
+      'numTwo' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_TWO,
+      'numThree' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_THREE,
+      'numFour' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_FOUR,
+      'numFive' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_FIVE,
+      'numSix' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_SIX,
+      'numSeven' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_SEVEN,
+      'numEight' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_EIGHT,
+      'numNine' : Phaser.Input.Keyboard.KeyCodes.NUMPAD_NINE,
+      'backspace' : Phaser.Input.Keyboard.KeyCodes.BACKSPACE
+    });
+
+    return Object.assign(otherKeys, arrowKeys);
   }
 };
