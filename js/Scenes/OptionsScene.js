14391bc8fa2eb6178d310d428918ae9204fcfda2
Sylvebois <thomasghaye@hotmail.com>
2020-01-27 16:59:21 +0100

    Started Option screen creation and multilingual support

--- js/Scenes/OptionsScene.js
+++ js/Scenes/OptionsScene.js
@@ -1,36 +1,75 @@
 import * as PHASER from '../phaser.min.js';
+import { interfaceText } from '../text.js';
 import config from '../Config/config.js';
 import Button from '../Objects/Button.js';
 
 export default class OptionsScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Options');
   }
 
-  create () {
-    this.model = this.sys.game.globals.model;
+  create() {
+    this.model = this.game.globals.model;
+    this.language = this.game.globals.language;
 
-    this.text = this.add.text(300, 100, 'Options', { fontSize: 40 });
-    this.musicButton = this.add.image(200, 200, 'checkedBox');
-    this.musicText = this.add.text(250, 190, 'Music Enabled', { fontSize: 24 });
+    // Background
+    this.add.image(config.width / 2, config.height / 2, 'bgScroll');
 
-    this.soundButton = this.add.image(200, 300, 'checkedBox');
-    this.soundText = this.add.text(250, 290, 'Sound Enabled', { fontSize: 24 });
+    // Title
+    let title = this.add.text(0, 0, interfaceText.buttons.fr[2], { fontSize: '40px' });
+    title.setPosition(config.width / 2 - title.width / 2, title.height * 3);
 
-    this.musicButton.setInteractive();
-    this.soundButton.setInteractive();
+    // Music
+    this.musicButton = this.add.image(200, 200, 'checkedBox').setInteractive();
+    this.musicText = this.add.text(this.musicButton.x + this.musicButton.width + 20, this.musicButton.y - 10, interfaceText.buttons[this.language][4], { fontSize: '24px' });
 
-    this.musicButton.on('pointerdown', function () {
+    // Sound
+    this.soundButton = this.add.image(200, 275, 'checkedBox').setInteractive();
+    this.soundText = this.add.text(this.soundButton.x + this.soundButton.width + 20, this.soundButton.y - 10, interfaceText.buttons[this.language][5], { fontSize: '24px' });
+
+    // Language selection
+    let frText = this.add.text(0, 0, 'FRA', { fontSize: '24px' });
+    frText.setPosition(config.width / 2 - frText.width - 5, 350);
+
+    let enText = this.add.text(0, 0, 'ENG', { fontSize: '24px' });
+    enText.setPosition(config.width / 2 + enText.width + 5, 350);
+
+    let languageText = this.add.text(150, 400, interfaceText.buttons[this.language][6], { fontSize: '24px' });
+
+    this.frButton = this.add.image(frText.x + frText.width / 2, languageText.y + 10, (this.game.globals.language === 'fr') ? 'checkedBox' : 'box').setInteractive();
+    this.enButton = this.add.image(enText.x + enText.width / 2, languageText.y + 10, (this.game.globals.language === 'en') ? 'checkedBox' : 'box').setInteractive();
+
+    // Back button
+    this.menuButton = new Button(this, config.width / 2, config.height - 100, 'titleButton', interfaceText.buttons[this.language][7], 'Title');
+
+    // Interactivity
+    this.musicButton.on('pointerdown', () => {
       this.model.musicOn = !this.model.musicOn;
       this.updateAudio();
-    }.bind(this));
+    });
 
-    this.soundButton.on('pointerdown', function () {
+    this.soundButton.on('pointerdown', () => {
       this.model.soundOn = !this.model.soundOn;
       this.updateAudio();
-    }.bind(this));
+    });
 
-    this.menuButton = new Button(this, 400, 500, 'blueButton1', 'blueButton2', 'Menu', 'Title');
+    this.frButton.on('pointerdown', () => {
+      if (this.game.globals.language !== 'fr') {
+        this.game.globals.language = 'fr';
+        this.frButton.setTexture('checkedBox');
+        this.enButton.setTexture('box');
+        this.scene.restart();
+      }
+    });
+
+    this.enButton.on('pointerdown', () => {
+      if (this.game.globals.language !== 'en') {
+        this.game.globals.language = 'en';
+        this.enButton.setTexture('checkedBox');
+        this.frButton.setTexture('box');
+        this.scene.restart();
+      }
+    });
 
     this.updateAudio();
   }
@@ -40,7 +79,8 @@ export default class OptionsScene extends Phaser.Scene {
       this.musicButton.setTexture('box');
       this.sys.game.globals.bgMusic.stop();
       this.model.bgMusicPlaying = false;
-    } else {
+    }
+    else {
       this.musicButton.setTexture('checkedBox');
       if (this.model.bgMusicPlaying === false) {
         this.sys.game.globals.bgMusic.play();
@@ -50,7 +90,8 @@ export default class OptionsScene extends Phaser.Scene {
 
     if (this.model.soundOn === false) {
       this.soundButton.setTexture('box');
-    } else {
+    }
+    else {
       this.soundButton.setTexture('checkedBox');
     }
   }
